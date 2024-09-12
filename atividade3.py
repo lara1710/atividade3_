04db87044024fcaac62dba2703a41d47447fec1c
lara1710 <laraleticia1203@gmail.com>
2024-09-12 01:08:28 -0300

    atividade

---
+++ atividade3.py
@@ -0,0 +1,10 @@
+# Crie um programa que calcule a área de um retângulo, pedindo ao usuário a largura e a altura.
+largura = int(input("digite os valores:"))
+
+altura = int(input("digite os valores:"))
+
+
+area  = largura * altura
+
+print(F"{area}")
+
